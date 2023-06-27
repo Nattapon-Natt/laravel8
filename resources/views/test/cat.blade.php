ad8165fb55f82ce059df8c023b0da193ef495c88
Nattapon-Natt <nonnynatt@gmail.com>
2023-06-27 17:25:44 +0700

    week2

---
+++ resources/views/test/cat.blade.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+<h1>Cat of ณัฐพล ไพโรจน์ 64122420008</h1>
+ 
+cat  <img src = "{{$cat}}" alt ="blackpanter" style ="width:500px; height:300px;">
+
+</body>
+</html>    
---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<h1>Gallery of ณัฐพล ไพโรจน์ 64122420008</h1>
+ 
+ant <a href = "/gallery/ant"><img src = "{{$ant}}" alt ="antman" style ="width:200px; height:100px;"></a>
+bird <a href = "/gallery/bird"> <img src = "{{$bird}}" alt ="falcon" style ="width:200px; height:100px;"></a>
+cat  <a href = "/gallery/cat"> <img src = "{{$cat}}" alt ="blackpanter" style ="width:200px; height:100px;"></a>
+god <a href = "/gallery/god"> <img src = "{{$god}}" alt ="thor" style ="width:200px; height:100px;"></a>
+spider <a href = "/gallery/spider"> <img src = "{{$spider}}" alt ="" style ="width:200px; height:100px;"></a>
+     
+</body>
+</html>    
