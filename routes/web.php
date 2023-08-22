f870a944f4b21327aff14e7389be8027c011c5c3
Nattapon-Natt <nonnynatt@gmail.com>
2023-08-22 16:53:21 +0700

    week9

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\MyProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Models\Staff;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;  //เขียนเพิ่ม
+use App\Http\Controllers\UserController;  //เขียนเพิ่ม
+use App\Http\Controllers\VehicleController;  //เขียนเพิ่ม
 
 /*
 |--------------------------------------------------------------------------
@@ -139,3 +143,13 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
+
+// Route::resource('post', 'PostController');
+Route::resource('post', PostController::class);
+Route::resource('profile', 'ProfileController');
+Route::resource('user', 'UserController');
+Route::resource('vehicle', 'VehicleController');
+
+Route::resource('profile', ProfileController::class);
+Route::resource('user', UserController::class);
+Route::resource('vehicle', VehicleController::class);
